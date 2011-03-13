213d142a4826fd3be2e2a5ec9434b77d5da373f5
Boris Dinkevich <reg@dinkevich.com>
2011-03-13 12:15:39 +0200

    Added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zait::Application.config.secret_token = 'ac90d59646912691bcaa2b03ff839065ae5bef55c22f0ab4b5d96e778384041360c432d2a0d28040b4a0d5e7dabf66711897978eba0107dec6efd2a75ca827e1'
